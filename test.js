90503d362d1b6b7e777da1adb98e17df9176adba
tom <tom@sapletta.com>
2021-05-05 14:26:09 +0200

    update puppeteer

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async() => {
     const browser = await puppeteer.launch({
         headless:false,
+        userDataDir: "./user_data2",
         args: ["--no-sandbox"]
     });
     console.log(await browser.version());
