4b38b1db7309895d72174d353f7d2d6b1336c33c
tom <tom@sapletta.com>
2021-05-05 23:47:02 +0200

    redirection

--- png.js
+++ png.js
@@ -177,14 +177,14 @@ async function capture(img, url, res) {
     ///////////
     const browser = await puppeteer.launch({
         headless: true,
-        timeout: 0,
-        ignoreHTTPSErrors: true,
+        // timeout: 0,
+        // ignoreHTTPSErrors: true,
         // ignoreDefaultArgs: ['--disable-extensions'],
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--ignore-certificate-errors',
-            '--disable-gpu',
+            // '--disable-gpu',
         ]
     });
 
